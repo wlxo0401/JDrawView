f2e5e1ec22a31e8955c8d177943afacacdf753c4
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2024-03-04 22:31:29 +0900

    test

---
+++ JDrawViewDemo/JDrawViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JDrawViewDemo
+//
+//  Created by KimJitae on 3/4/24.
+//
+
+import UIKit
+import JDrawView
+
+class ViewController: UIViewController {
+
+    let test: JDrawView = JDrawView(name: "김지태")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        test.heee()
+        
+    }
+
+
+}
+
