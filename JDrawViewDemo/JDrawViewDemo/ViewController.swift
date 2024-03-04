621be3876ee1770d215893f404a99144484e4a0b
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2024-03-04 23:26:31 +0900

    test

--- JDrawViewDemo/JDrawViewDemo/ViewController.swift
+++ JDrawViewDemo/JDrawViewDemo/ViewController.swift
@@ -10,18 +10,31 @@ import JDrawView
 
 class ViewController: UIViewController {
 
-    let test: JDrawView = JDrawView(name: "김지태")
+    let jdraw: JDrawView = JDrawView(background: .blue)
     
+    @IBOutlet weak var storyboardJDrawView: JDrawView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        storyboardJDrawView.backgroundColor = .red
+        
+        self.view.addSubview(jdraw)
+        
+        jdraw.translatesAutoresizingMaskIntoConstraints = false
         
         
-        test.heee()
+        // Set the height constraint to 300
+        jdraw.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
+        // Set leading constraint to 10
+        jdraw.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+        
+        // Set trailing constraint to -10 (to ensure 10pt spacing from the trailing edge)
+        jdraw.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        
+        // Set bottom constraint to -10 (to ensure 10pt spacing from the bottom edge)
+        jdraw.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
     }
-
-
 }
 
--- Sources/JDrawView/JDrawView.swift
+++ Sources/JDrawView/JDrawView.swift
@@ -1,18 +1,35 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public class JDrawView {
+import UIKit
+
+public class JDrawView: UIView {
     
-    
-    var name: String = ""
-    
-    public init(name: String) {
-        self.name = name
+    public init(background: UIColor) {
+        super.init(frame: .zero)
+        
+            self.backgroundColor = background
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    
-    public func heee() {
-        print("이름: \(name)")
+    public override func draw(_ rect: CGRect) {
+        print("draw")
+        return
+    }
+
+    public override func touchesBegan(_ touches: Set<UITouch>, 
+                               with event: UIEvent?) {
+        print("touchesBegan")
+        return
+    }
+
+    public override func touchesMoved(_ touches: Set<UITouch>, 
+                               with event: UIEvent?) {
+        print("touchesMoved")
+        return
     }
     
 }
