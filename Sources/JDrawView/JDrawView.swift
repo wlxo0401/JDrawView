b5041c5c4f86692c3ea8ceda3fce44b181cc06f3
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2024-03-04 22:14:42 +0900

    test

---
+++ Sources/JDrawView/JDrawView.swift
@@ -0,0 +1,18 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class JDrawView {
+    
+    
+    var name: String = ""
+    
+    public init(name: String) {
+        self.name = name
+    }
+    
+    
+    func heee() {
+        print("이름: \(name)")
+    }
+    
+}
