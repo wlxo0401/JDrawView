1400fb613916cd6ea527d3d24a53b579ed7f1080
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2024-03-05 21:26:54 +0900

    base

--- JDrawViewDemo/JDrawViewDemo/ViewController.swift
+++ JDrawViewDemo/JDrawViewDemo/ViewController.swift
@@ -10,31 +10,27 @@ import JDrawView
 
 class ViewController: UIViewController {
 
-    let jdraw: JDrawView = JDrawView(background: .blue)
+    let jdraw: JDrawView = JDrawView()
     
     @IBOutlet weak var storyboardJDrawView: JDrawView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         storyboardJDrawView.backgroundColor = .red
         
-        self.view.addSubview(jdraw)
-        
-        jdraw.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        // Set the height constraint to 300
-        jdraw.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        
-        // Set leading constraint to 10
-        jdraw.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
-        
-        // Set trailing constraint to -10 (to ensure 10pt spacing from the trailing edge)
-        jdraw.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
-        
-        // Set bottom constraint to -10 (to ensure 10pt spacing from the bottom edge)
-        jdraw.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -10).isActive = true
+    }
+    
+    // 움직임
+    public override func touchesMoved(_ touches: Set<UITouch>,
+                                      with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {
+            return
+        }
+                
+        print("sdfdfpoint \(point)")
+        return
     }
 }
 
--- Sources/JDrawView/JDrawView.swift
+++ Sources/JDrawView/JDrawView.swift
@@ -3,33 +3,110 @@
 
 import UIKit
 
-public class JDrawView: UIView {
-    
-    public init(background: UIColor) {
-        super.init(frame: .zero)
-        
-            self.backgroundColor = background
-    }
+@objc
+public protocol JDrawViewDelegate: AnyObject {
+    @objc optional func touchesBeganJD()
+    @objc optional func touchesMovedJD()
+    @objc optional func getDrawPosition(position: CGPoint)
+}
 
+public class JDrawView: UIView {
+    // Line Struct
+    struct Line {
+        let storkeWidth: Float
+        let color: UIColor
+        var points: [CGPoint]
+    }
+    
+    // Line
+    var lines = [Line]()
+    
+    private var strokeColor = UIColor.black
+    private var strokeWidth: Float = 1
+    
+    var delegate: JDrawViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    public override func draw(_ rect: CGRect) {
-        print("draw")
-        return
-    }
-
-    public override func touchesBegan(_ touches: Set<UITouch>, 
-                               with event: UIEvent?) {
-        print("touchesBegan")
-        return
-    }
-
-    public override func touchesMoved(_ touches: Set<UITouch>, 
-                               with event: UIEvent?) {
-        print("touchesMoved")
-        return
+    // Line Width
+    func setLineWidth(width: Float){
+        self.strokeWidth = width
     }
     
+    // Line Color
+    func setLineColor(color: UIColor) {
+        self.strokeColor = color
+    }
+}
+
+// MARK: - Draw
+extension JDrawView {
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+             
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+         
+        self.lines.forEach { (line) in
+            for (i,p) in line.points.enumerated() {
+                if i == 0 { // first index
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+            context.strokePath()
+        }
+        return
+    }
+
+    // Began
+    public override func touchesBegan(_ touches: Set<UITouch>,
+                                      with event: UIEvent?) {
+        self.delegate?.touchesBeganJD?()
+        self.lines.append(Line.init(storkeWidth: 2,
+                               color: .red,
+                               points: []))
+        
+        return
+    }
+
+    // Move
+    public override func touchesMoved(_ touches: Set<UITouch>,
+                                      with event: UIEvent?) {
+        guard let point = touches.first?.location(in: self) else {
+            return
+        }
+        
+        self.delegate?.touchesMovedJD?()
+        self.delegate?.getDrawPosition?(position: point)
+        
+        guard var lastLine = self.lines.popLast() else {
+            return
+        }
+        
+        lastLine.points.append(point)
+        
+        self.lines.append(lastLine)
+        
+        setNeedsDisplay()
+        return
+    }
+}
+
+// MARK: - Util
+extension JDrawView {
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
 }
