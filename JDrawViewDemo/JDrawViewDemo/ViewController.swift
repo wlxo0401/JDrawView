bd5a6c6e399a7b003bceae3cef4ab682292b7b28
KimJiTae <wlxo0401@gmail.com>
2024-09-02 18:52:03 +0900

    add Alert

--- JDrawViewDemo/JDrawViewDemo/ViewController.swift
+++ JDrawViewDemo/JDrawViewDemo/ViewController.swift
@@ -36,6 +36,16 @@ class ViewController: UIViewController, JDrawViewDelegate {
     @IBAction func asUIImageButton(_ sender: Any) {
         let image = storyboardJDrawView.asImage()
         UIPasteboard.general.image = image
+        
+        let alert = UIAlertController(title: "Conversion Complete",
+                                      message: "The image conversion has been completed.",
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        // Assuming this is within a view controller
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
