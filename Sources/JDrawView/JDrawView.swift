010edf1cd52f7e90b120baa60f4fc56b58409f05
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2024-09-10 22:51:07 +0900

    라이브러리 개선

--- Sources/JDrawView/JDrawView.swift
+++ Sources/JDrawView/JDrawView.swift
@@ -1,64 +1,184 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  JDrawView.swift
+//  Bmea
+//
+//  Created by 김지태 on 9/3/24.
+//
 
 import UIKit
 
-@objc
-public protocol JDrawViewDelegate: AnyObject {
-    @objc optional func touchesBeganJD()
-    @objc optional func touchesMovedJD()
-    @objc optional func getDrawPosition(position: CGPoint)
-}
-
+// Main drawing view class
 public class JDrawView: UIView {
-    // Line Struct
+    // Struct to represent a line in the drawing
     struct Line {
-        let storkeWidth: Float
+        let strokeWidth: CGFloat
         let color: UIColor
         var points: [CGPoint]
     }
     
-    // Line
-    var lines = [Line]()
+    // Layer responsible for actual drawing
+    private let drawingLayer: DrawingLayer
     
-    private var strokeColor = UIColor.black
-    private var strokeWidth: Float = 1
+    // Variables for dash lines
+    private var horizontalDashLine: CAShapeLayer?
+    private var verticalDashLine: CAShapeLayer?
     
-    public var delegate: JDrawViewDelegate?
+    // Drawing color with property observer
+    private var drawColor: UIColor {
+        didSet {
+            self.drawingLayer.setDrawColor(color: self.drawColor)
+        }
+    }
     
+    // Drawing width with property observer
+    private var drawWidth: CGFloat {
+        didSet {
+            self.drawingLayer.setDrawWidth(width: self.drawWidth)
+        }
+    }
+    
+    // Initializers
     override init(frame: CGRect) {
+        self.drawingLayer = DrawingLayer(frame: frame)
+        self.drawColor = .black
+        self.drawWidth = 5
         super.init(frame: frame)
-        self.backgroundColor = .white
+        self.commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
+        self.drawingLayer = DrawingLayer(frame: .zero)
+        self.drawColor = .black
+        self.drawWidth = 1
         super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    // Common initialization method
+    private func commonInit() {
         self.backgroundColor = .white
+        self.drawingLayer.backgroundColor = .clear
+        self.addSubview(self.drawingLayer)
+        self.drawingLayer.setDrawColor(color: self.drawColor)
+        self.drawingLayer.setDrawWidth(width: self.drawWidth)
     }
     
-    // Line Width
-    public func setLineWidth(width: Float){
-        self.strokeWidth = width
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        self.drawingLayer.frame = self.bounds
+            
+        // Update dash lines if they exist
+        self.updateDashLines()
     }
-    
-    // Line Color
-    public func setLineColor(color: UIColor) {
-        self.strokeColor = color
+        
+    // New method to update dash lines
+    private func updateDashLines() {
+        if let horizontalLine = self.horizontalDashLine {
+            horizontalLine.removeFromSuperlayer()
+            self.addDashedLine(from: CGPoint(x: 0, y: self.frame.size.height / 2),
+                          to: CGPoint(x: self.frame.size.width, y: self.frame.size.height / 2),
+                          to: horizontalLine)
+        }
+            
+        if let verticalLine = self.verticalDashLine {
+            verticalLine.removeFromSuperlayer()
+            self.addDashedLine(from: CGPoint(x: self.frame.size.width / 2, y: 0),
+                          to: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height),
+                          to: verticalLine)
+        }
     }
 }
 
-// MARK: - Draw
+// MARK: - Internal Methods
 extension JDrawView {
-    public override func draw(_ rect: CGRect) {
+    private func addDashedLine(from start: CGPoint, to end: CGPoint, to lineLayer: CAShapeLayer) {
+        let linePath = UIBezierPath()
+        linePath.move(to: start)
+        linePath.addLine(to: end)
+
+        lineLayer.path = linePath.cgPath
+        lineLayer.strokeColor = UIColor.gray.cgColor
+        lineLayer.lineWidth = 1
+        lineLayer.lineDashPattern = [6, 3]
+
+        self.layer.addSublayer(lineLayer)
+    }
+}
+
+// MARK: - Public Methods
+extension JDrawView {
+    // Convert the drawing to an image
+    public func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: self.drawingLayer.bounds)
+        return renderer.image { rendererContext in
+            self.drawingLayer.layer.render(in: rendererContext.cgContext)
+        }
+    }
+    
+    // Copy the drawing as an image to the clipboard
+    public func copyImage() {
+        let image = self.asImage()
+        UIPasteboard.general.image = image
+    }
+    
+    public func setHorizonDashLine(set: Bool) {
+        if set {
+            if self.horizontalDashLine == nil {
+                self.horizontalDashLine = CAShapeLayer()
+            }
+        } else {
+            self.horizontalDashLine?.removeFromSuperlayer()
+            self.horizontalDashLine = nil
+        }
+        self.updateDashLines()
+    }
+        
+    public func setVerticalDashLine(set: Bool) {
+        if set {
+            if self.verticalDashLine == nil {
+                self.verticalDashLine = CAShapeLayer()
+            }
+        } else {
+            self.verticalDashLine?.removeFromSuperlayer()
+            self.verticalDashLine = nil
+        }
+        self.updateDashLines()
+    }
+    
+    // Set the drawing line width
+    public func setDrawWidth(width: CGFloat) {
+        self.drawWidth = width
+    }
+    
+    // Set the drawing color
+    public func setDrawColor(color: UIColor) {
+        self.drawColor = color
+    }
+    
+    // Clear the drawing
+    public func clearDrawing() {
+        self.drawingLayer.clear()
+    }
+}
+
+// MARK: - Drawing Layer
+class DrawingLayer: UIView {
+    private var lines = [JDrawView.Line]()
+    private var drawColor: UIColor = .black
+    private var drawWidth: CGFloat = 1
+    
+    // Draw the lines
+    override func draw(_ rect: CGRect) {
         super.draw(rect)
              
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
+        guard let context = UIGraphicsGetCurrentContext() else { return }
          
-        self.lines.forEach { (line) in
-            for (i,p) in line.points.enumerated() {
-                if i == 0 { // first index
+        self.lines.forEach { line in
+            context.setStrokeColor(line.color.cgColor)
+            context.setLineWidth(line.strokeWidth)
+            
+            for (i, p) in line.points.enumerated() {
+                if i == 0 {
                     context.move(to: p)
                 } else {
                     context.addLine(to: p)
@@ -66,49 +186,36 @@ extension JDrawView {
             }
             context.strokePath()
         }
-        return
     }
-
-    // Began
-    public override func touchesBegan(_ touches: Set<UITouch>,
-                                      with event: UIEvent?) {
-        self.delegate?.touchesBeganJD?()
-        self.lines.append(Line.init(storkeWidth: 2,
-                               color: .red,
-                               points: []))
-        
-        return
+    
+    // Start a new line when touch begins
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.lines.append(JDrawView.Line(strokeWidth: self.drawWidth, color: self.drawColor, points: []))
     }
-
-    // Move
-    public override func touchesMoved(_ touches: Set<UITouch>,
-                                      with event: UIEvent?) {
-        guard let point = touches.first?.location(in: self) else {
-            return
-        }
-        
-        self.delegate?.touchesMovedJD?()
-        self.delegate?.getDrawPosition?(position: point)
-        
-        guard var lastLine = self.lines.popLast() else {
-            return
-        }
+    
+    // Add points to the current line as touch moves
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: self),
+              var lastLine = self.lines.popLast() else { return }
         
         lastLine.points.append(point)
-        
         self.lines.append(lastLine)
-        
-        setNeedsDisplay()
-        return
+        self.setNeedsDisplay()
     }
-}
-
-// MARK: - Util
-extension JDrawView {
-    public func asImage() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: bounds)
-        return renderer.image { rendererContext in
-            layer.render(in: rendererContext.cgContext)
-        }
+    
+    // Set the drawing width
+    func setDrawWidth(width: CGFloat) {
+        self.drawWidth = width
+    }
+    
+    // Set the drawing color
+    func setDrawColor(color: UIColor) {
+        self.drawColor = color
+    }
+    
+    // Clear all lines
+    func clear() {
+        self.lines.removeAll()
+        self.setNeedsDisplay()
     }
 }
